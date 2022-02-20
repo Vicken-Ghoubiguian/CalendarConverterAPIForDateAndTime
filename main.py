992a2235d691fc0581542157ba8aa886008ce873
Wicken <ericghoubiguian@live.fr>
2022-02-20 10:28:28 +0100

    Adding '#' for comments...

--- main.py
+++ main.py
@@ -1,15 +1,23 @@
+#
 from flask import Flask, request
 from flask_restx import Api, Resource, reqparse
 
+#
 app = Flask(__name__)
 
+#
 api = Api(app)
 
+#
 @api.route('/hello')
 class HelloWorld(Resource):
 
+    #
     def get(self):
         return 'Hello, world!'
 
+#
 if __name__ == '__main__':
+    
+    #
     app.run()
