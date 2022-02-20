e3b21df28b6cabf51b46cd310dfc33132234492d
Wicken <ericghoubiguian@live.fr>
2022-02-20 10:21:33 +0100

    Simple 'Hello, world !' to complex 'Hello, world !'...

--- main.py
+++ main.py
@@ -1,1 +1,15 @@
-print('Hello, world!')
+from flask import Flask, request
+from flask_restx import Api, Resource, reqparse
+
+app = Flask(__name__)
+
+api = Api(app)
+
+@api.route('/hello')
+class HelloWorld(Resource):
+
+    def get(self):
+        return 'Hello, world!'
+
+if __name__ == '__main__':
+    app.run()
