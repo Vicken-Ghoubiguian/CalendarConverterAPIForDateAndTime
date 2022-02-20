8f533e242b2969b46ed35970725d90b5ac0b0a99
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-20 11:54:42 +0100

    Sommuy

--- main.py
+++ main.py
@@ -23,23 +23,28 @@ class HelloWorld(Resource):
         return {"Message": "Hello, world!"}, 200
 
 #
-@api.route('/currentDateTime/<string:choosen_timezone>')
+parser_current_date_and_time_by_timezone = reqparse.RequestParser()
+parser_current_date_and_time_by_timezone.add_argument('timezone', type=str, required=True, help='')
+
+#
+@api.route('/currentDateTime')
+@api.expect(parser_current_date_and_time_by_timezone)
 class CurrentDateTimeByTimezone(Resource):
 
     #
-    def get(self, choosen_timezone):
+    def get(self):
+
+        #
+        args = parser_current_date_and_time_by_timezone.parse_args()
 
         #
         now_utc = datetime.now(timezone('UTC'))
 
         #
-        choosen_timezone = choosen_timezone.replace("&", "/")
+        now_from_timezone = now_utc.astimezone(timezone(args["timezone"]))
 
         #
-        now_from_timezone = now_utc.astimezone(timezone(choosen_timezone))
-
-        #
-        return {"time": now_from_timezone.strftime(date_and_time_template), "timezone": choosen_timezone}, 200
+        return {"time": now_from_timezone.strftime(date_and_time_template), "timezone": args["timezone"]}, 200
 
 #
 if __name__ == '__main__':
