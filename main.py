580edf7d37d3e2a2eed1be789a02b3ac217490ec
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-26 14:57:42 +0100

    HHJJHJ

--- main.py
+++ main.py
@@ -1,70 +1,22 @@
 #
 from flask import Flask, request
-from flask_restx import Api, Resource, reqparse
-from datetime import datetime
-from pytz import timezone
+from flask_restx import Api
+from timezones import currentDateNamespace as nstz
 
 #
 app = Flask(__name__)
 
-#
-api = Api(app)
+api = Api(
+    title='CalendarConverterAPIForDateAndTime',
+    version='1.0',
+    description='A converter API for date and time in many calendars in the world (Gregorian, Julian, Chinese, Solar Hijri, Maya, Republican...)...'
+)
 
 #
-parser_current_date_and_time_by_timezone = reqparse.RequestParser()
-parser_current_date_and_time_by_timezone.add_argument('timezone', type=str, required=True, help='Enter here the IANA (Internet Assigned Numbers Authority) timezone...')
+api.add_namespace(nstz)
 
 #
-@api.route('/currentDateTime')
-@api.expect(parser_current_date_and_time_by_timezone)
-class CurrentDateTimeByTimezone(Resource):
-
-    #
-    def get(self):
-
-        #
-        try:
-
-            #
-            date_and_time_template = "%Y-%m-%d %H:%M:%S"
-
-            #
-            args = parser_current_date_and_time_by_timezone.parse_args()
-
-            #
-            now_utc = datetime.now(timezone('UTC'))
-
-            #
-            now_from_timezone = now_utc.astimezone(timezone(args["timezone"]))
-
-            #
-            return {
-                    "time": now_from_timezone.strftime(date_and_time_template), 
-                    "timezone": args["timezone"],
-                    "lat": None,
-                    "lon": None
-                    }, 200
-
-        #
-        except Exception as exp:
-
-            #
-            return {"error": exp.string()}, 200
-
-#
-parser_current_date_and_time_by_timezone_in_particular_calendar = parser_current_date_and_time_by_timezone.copy()
-
-
-#
-@api.route('/currentDate/particularCalendar')
-@api.expect(parser_current_date_and_time_by_timezone_in_particular_calendar)
-class CurrentDateTimeByTimezoneInParticularCalendar(Resource):
-
-    #
-    def get(self):
-
-        #
-        return {"TODO": "TODO"}, 200
+api.init_app(app)
 
 #
 if __name__ == '__main__':
