33b0f2a0f9dadd1fbcfc556d05c4a6b535660494
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-27 12:12:13 +0100

    NBOSS 2

--- timezones.py
+++ timezones.py
@@ -7,6 +7,18 @@ from pycountry import countries
 currentTimezonesNamespace = Namespace('timezones', description='Namespace to manipulate and get some informations about timezones...')
 
 #
+@currentTimezonesNamespace.route('/infos')
+class CurrentTimezonesInfos(Resource):
+
+    #@currentDateTimeNamespace.doc('list_cats')
+
+    #
+    def get(self):
+
+        #
+        return {"TODO": "TODO"}, 200
+
+#
 @currentTimezonesNamespace.route('/byCountries')
 class CurrentTimezonesByCountries(Resource):
 
