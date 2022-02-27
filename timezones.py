b7070fdc6493356249d89ee638f118e17677c025
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-27 10:07:09 +0100

    Delete Error for future...

--- timezones.py
+++ timezones.py
@@ -11,8 +11,7 @@ parser_current_date_and_time_name_space = reqparse.RequestParser()
 parser_current_date_and_time_name_space.add_argument('timezone', type=str, required=True, help='Enter here the IANA (Internet Assigned Numbers Authority) timezone...')
 
 #
-parser_current_date_and_time_by_timezone = reqparse.RequestParser()
-parser_current_date_and_time_by_timezone.add_argument('timezone', type=str, required=True, help='Enter here the IANA (Internet Assigned Numbers Authority) timezone...')
+parser_current_date_and_time_by_timezone = parser_current_date_and_time_name_space.copy()
 
 #
 @currentDateTimeNamespace.route('')
@@ -25,36 +24,27 @@ class CurrentDateTimeByTimezone(Resource):
     def get(self):
 
         #
-        try:
+        date_and_time_template = "%Y-%m-%d %H:%M:%S"
 
-            #
-            date_and_time_template = "%Y-%m-%d %H:%M:%S"
+        #
+        args = parser_current_date_and_time_by_timezone.parse_args()
 
-            #
-            args = parser_current_date_and_time_by_timezone.parse_args()
+        #
+        now_utc = datetime.now(timezone('UTC'))
 
-            #
-            now_utc = datetime.now(timezone('UTC'))
+        #
+        now_from_timezone = now_utc.astimezone(timezone(args["timezone"]))
 
-            #
-            now_from_timezone = now_utc.astimezone(timezone(args["timezone"]))
-
-            #
-            return {
+        #
+        return {
                     "time": now_from_timezone.strftime(date_and_time_template), 
                     "timezone": args["timezone"],
                     "lat": None,
                     "lon": None
-                    }, 200
-
-        #
-        except Exception as exp:
-
-            #
-            return {"error": exp.string()}, 200
+                }, 200
 
 #
-parser_current_date_and_time_by_timezone_in_particular_calendar = parser_current_date_and_time_by_timezone.copy()
+parser_current_date_and_time_by_timezone_in_particular_calendar = parser_current_date_and_time_name_space.copy()
 
 
 #
