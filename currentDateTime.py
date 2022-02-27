5bbe610924ae8a071cd2c6913cecfd96243941b5
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-27 12:26:02 +0100

    NBOSS 5 bis.

--- currentDateTime.py
+++ currentDateTime.py
@@ -44,8 +44,10 @@ class CurrentDateTimeByTimezone(Resource):
         return {
                     "date_and_time": now_from_timezone.strftime(date_and_time_template), 
                     "timezone": args["timezone"],
-                    "country_name": countries.get(alpha_2=getCountryCodeOfTimezone(args["timezone"])).name,
-                    "country_code": getCountryCodeOfTimezone(args["timezone"]),
+                    "country": {
+                        "country_name": countries.get(alpha_2=getCountryCodeOfTimezone(args["timezone"])).name,
+                        "country_code": getCountryCodeOfTimezone(args["timezone"])
+                    },
                     "format": None
                 }, 200
 
