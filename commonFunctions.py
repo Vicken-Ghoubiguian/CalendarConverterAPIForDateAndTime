472b75a1520088e5748251379454f4b174de0f8b
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-02-27 12:40:34 +0100

    HJK 4

--- commonFunctions.py
+++ commonFunctions.py
@@ -49,28 +49,3 @@ def getCountry(timezone):
                 "country_name": countryName[0],
                 "country_code": countryCode
            }
-
-#
-def getCountryCodeOfTimezone(timezone):
-
-    #
-    timezones_list = country_timezones
-
-    #
-    timezone_index = -1
-
-    #
-    keys_from_timezones_list = list(timezones_list.keys())
-    values_from_timezones_list = list(timezones_list.values())
-
-    #
-    for current_timezone_array in values_from_timezones_list:
-
-        #
-        if timezone in current_timezone_array:
-
-            #
-            timezone_index = values_from_timezones_list.index(current_timezone_array)
-
-    #
-    return keys_from_timezones_list[timezone_index]
