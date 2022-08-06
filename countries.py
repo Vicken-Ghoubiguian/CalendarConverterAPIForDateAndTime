de9d792b4d4352299a2f74d6c933dfa8df42eef3
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-08-06 08:12:23 +0200

    JKIIO

--- countries.py
+++ countries.py
@@ -6,3 +6,17 @@ from commonFunctions import *
 
 #
 currentCountriesNamespace = Namespace('countries', description='Namespace to manipulate and get informations about all countries in the world...')
+
+#
+@currentCountriesNamespace.route('/list')
+class CountriesList(Resource):
+
+    #
+    def get(self):
+
+        """
+        
+        """
+
+        #
+        return {"TODO": "TODO"}, 200
