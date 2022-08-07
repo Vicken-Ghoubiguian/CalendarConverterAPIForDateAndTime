d31137d43e0888fb4956c6f563cbf53d75b06d9f
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-08-07 19:56:09 +0200

    JJK

--- countries.py
+++ countries.py
@@ -23,4 +23,4 @@ class CountriesList(Resource):
         print("\n\n\n\n\n" + str(pycountry.countries) + "\n\n\n\n\n")
 
         #
-        return getJSONOfCountries(len(pycountry.countries)), 200
+        return getJSONOfCountries(list(pycountry.countries)), 200
