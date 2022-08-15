b498e091f8d7ba315b43262c8d4959a2a42c185c
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-08-15 15:16:28 +0200

    J?

--- countries.py
+++ countries.py
@@ -52,7 +52,7 @@ class CountriesSortName(Resource):
     def get(self):
 
         """
-        Get the list of all ancient countries selected according a to pattern and sort according a ...
+        Get the list of all ancient countries selected according a to pattern and sort according to an order...
         """
 
         #
