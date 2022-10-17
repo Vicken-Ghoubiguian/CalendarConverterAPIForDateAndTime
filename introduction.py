1940a949301f785f200ff5b07f930161fb09c329
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-10-17 19:00:33 +0200

    OOOPPPP

--- introduction.py
+++ introduction.py
@@ -5,7 +5,7 @@ from flask_restx import Namespace, Resource, inputs
 from commonFunctions import *
 
 #
-introductionNamespace = Namespace('introduction', description='Namespace to introduce, present and explain all functionalities of the DateTime API...')
+introductionNamespace = Namespace('introduction', description='Namespace to introduce, present, explain all functionalities of the DateTime API and how it works...')
 
 #
 @introductionNamespace.route('/presentation')
