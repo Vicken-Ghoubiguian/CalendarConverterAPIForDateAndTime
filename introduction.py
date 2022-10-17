f390f1faaf6f46f3af8f052c5713f2ad7e12f3b7
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-10-17 19:22:59 +0200

    KLLKLK

--- introduction.py
+++ introduction.py
@@ -23,4 +23,4 @@ class Presentation(Resource):
         headers = {"Content-Type": "text/html"}
 
         #
-        return make_response('<b>Test</b>',200,headers)
+        return make_response(render_template('index.html'), 200, headers)
