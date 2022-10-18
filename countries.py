8512c1ae46d9c0e224744028277665884c8ea5d2
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-10-18 15:10:04 +0200

    OPOPOP

--- countries.py
+++ countries.py
@@ -9,7 +9,7 @@ from commonFunctions import *
 currentCountriesNamespace = Namespace('countries', description='Namespace to manipulate and get informations about all countries in the world...')
 
 #
-@currentCountriesNamespace.route('/list')
+@currentCountriesNamespace.route('')
 class CountriesList(Resource):
 
     #
@@ -23,7 +23,7 @@ class CountriesList(Resource):
         return getJSONOfCountries(list(pycountry.countries)), 200
 
 #
-@currentCountriesNamespace.route('/list/historical')
+@currentCountriesNamespace.route('/historical')
 class HistoricalCountriesList(Resource):
 
     #
