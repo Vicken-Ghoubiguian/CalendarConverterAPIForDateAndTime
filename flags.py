70926ee98bf098468f3504b0579720d9b9505dcd
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-11-14 08:59:09 +0100

    KLKLK

--- flags.py
+++ flags.py
@@ -6,3 +6,16 @@ from commonFunctions import *
 
 #
 flagsNamespace = Namespace('flags', description='Namespace to ...')
+
+#
+@flagsNamespace.route('')
+class flagByCountry(Resource):
+
+    #
+    def post(self):
+
+        """
+        """
+
+        #
+        print("")
