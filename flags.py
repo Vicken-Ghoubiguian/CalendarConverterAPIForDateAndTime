2c70518ab4ffc7caf889517f933c6d82a6ba59f5
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-12-01 12:10:47 +0100

    QsQ

--- flags.py
+++ flags.py
@@ -15,6 +15,9 @@ parser_flags = reqparse.RequestParser()
 parser_flags.add_argument('country', type=str, required=True, choices=getAllCountriesForFlags(), help='Select the country whose flag and data you want...')
 
 #
+#parser_flags.add_argument('format', type=str, required=True, choices=['.png', '.webp', '.svg', '.jpeg'], help='')
+
+#
 @flagsNamespace.route('')
 @flagsNamespace.expect(parser_flags)
 class flagByCountry(Resource):
