55581369fa81a391ebf9a025b038d5b546456567
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-12-01 12:52:33 +0100

    jkj

--- flags.py
+++ flags.py
@@ -15,9 +15,6 @@ parser_flags = reqparse.RequestParser()
 parser_flags.add_argument('country', type=str, required=True, choices=getAllCountriesForFlags(), help='Select the country whose flag and data you want...')
 
 #
-parser_flags.add_argument('format', type=str, required=True, choices=['.png', '.webp', '.svg', '.jpeg'], help='')
-
-#
 @flagsNamespace.route('')
 @flagsNamespace.expect(parser_flags)
 class FlagByCountry(Resource):
@@ -33,10 +30,10 @@ class FlagByCountry(Resource):
         args = parser_flags.parse_args()
 
         #
-        wavingFlagURL = "https://flagcdn.com/192x144/" + getCountryAlpha2FromCountryName(args["country"]) + args["format"]
+        wavingFlagURL = "https://flagcdn.com/192x144/" + getCountryAlpha2FromCountryName(args["country"]) + ".png"
 
         #
-        originalFlagURL = "https://flagcdn.com/h120/" + getCountryAlpha2FromCountryName(args["country"]) + args["format"]
+        originalFlagURL = "https://flagcdn.com/h120/" + getCountryAlpha2FromCountryName(args["country"]) + ".png"
 
         #
         countryName = args["country"]
@@ -48,8 +45,12 @@ class FlagByCountry(Resource):
         return make_response(render_template('flagByCountry.html', wavingFlagURL=wavingFlagURL, originalFlagURL=originalFlagURL, countryName=countryName), 200, headers)
 
 #
+parser_flags_to_download_flag = parser_flags.copy()
+parser_flags_to_download_flag.add_argument('format', type=str, required=True, choices=['.png', '.webp', '.svg', '.jpeg'], help='')
+
+#
 @flagsNamespace.route('/download')
-@flagsNamespace.expect(parser_flags)
+@flagsNamespace.expect(parser_flags_to_download_flag)
 class DownloadFlagByCountry(Resource):
 
     #
