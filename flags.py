597eb396565a3d9e541e5934977c7789220051b3
Eric ghoubiguian <eric.ghoubiguian@imerir.com>
2022-12-03 14:11:05 +0100

    PP

--- flags.py
+++ flags.py
@@ -86,4 +86,4 @@ class DownloadFlagByCountry(Resource):
         except ConnectionError:
 
             #
-            return {"code": 400, "Error": ""}, 400
+            return {"code": 400, "Error": "You are not connected to the Internet"}, 400
